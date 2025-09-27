a26aca832408fdcbc5576835a8f9ecc352a558fc
astrixity <astrixity72@gmail.com>
2025-09-27 21:59:24 +0800

    Update abc.py
    Signed-off-by: astrixity <astrixity72@gmail.com>

--- discord/abc.py
+++ discord/abc.py
@@ -1979,7 +1979,6 @@ class Connectable(Protocol):
         timeout: float = 60.0,
         reconnect: bool = True,
         cls: Callable[[Client, Connectable], T] = VoiceClient,
-        sink=None
     ) -> T:
         """|coro|
 
@@ -2022,7 +2021,7 @@ class Connectable(Protocol):
             raise ClientException("Already connected to a voice channel.")
 
         client = state._get_client()
-        voice = cls(client, self, sink=sink)
+        voice = cls(client, self)
 
         if not isinstance(voice, VoiceProtocol):
             raise TypeError("Type must meet VoiceProtocol abstract base class.")
